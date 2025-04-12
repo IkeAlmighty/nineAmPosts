b7f7968e3054aa908c7550ecc4153571f37ce2ef
scirrincione <sccirrincione@gmail.com>
2025-04-12 15:21:05 -0500

    add_route

--- server/src/routes/dailys.js
+++ server/src/routes/dailys.js
@@ -11,5 +11,11 @@ router.get('/:username', (req, res) => {
 })
 
 // TODO: create similar routes for other daily related tasks the frontend will need
+router.get('/:post', (req, res) => {
+    const { Daily } = req;
+    const allPosts = Daily.find({ posts });
+
+    res.json(allPosts);
+})
 
 export default router;
