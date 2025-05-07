055e822aeb7ea9ff4441d5c8b9a9993dca018b00
ikealmighty <isaacyates7@gmail.com>
2025-05-06 19:13:00 -0500

    Added date, previous, and next to daily object to support linked list dailys

--- server/src/models/daily.js
+++ server/src/models/daily.js
@@ -1,11 +1,36 @@
-import mongoose, { Schema, model, Document } from 'mongoose';
+import mongoose, { Schema, model, Document } from "mongoose";
 
 const dailySchema = new Schema({
-    user_id: Schema.Types.ObjectId,
-    posts: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Post' }],
-    boosts: [{user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }, count: Number}]
-},
-{ timestamps: true })
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  posts: [{ type: mongoose.Schema.Types.ObjectId, ref: "Post" }],
+  boosts: [
+    {
+      user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+      count: Number,
+    },
+  ],
+  date: {
+    // always set to midnight
+    type: mongoose.Schema.Types.Date,
+    required: true,
+  },
+  previous: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Daily",
+  },
+  next: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Daily",
+  },
+});
+
+dailySchema.pre("save", function (next) {
+  this.date = getMidnight(this.date);
+  next();
+});
 
 const Daily = mongoose.model("Daily", dailySchema);
 export default Daily;
