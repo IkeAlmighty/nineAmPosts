ff9bb6e9ddba70c78c0d272628111b28b791bff3
ikealmighty <isaacyates7@gmail.com>
2025-05-06 19:14:13 -0500

    started the pre find hook that will keep the daily field up to date

--- server/src/models/user.js
+++ server/src/models/user.js
@@ -1,35 +1,44 @@
-import mongoose, { Schema, model, Document } from 'mongoose';
+import mongoose, { Schema, model, Document } from "mongoose";
 
 const userSchema = new Schema({
-    username: {type: String, required: true, unique: true},
-    email: {type: String, required: true, unique: true},
-    password: {type: String, required: true},
-    followers: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
-    following: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
-    dailys: [{ type: Schema.Types.ObjectId, ref: 'Daily' }],
-    boosts: Number
-})
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  followers: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+  following: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+  daily: { type: Schema.Types.ObjectId, ref: "Daily" },
+  boosts: Number,
+});
 
-userSchema.pre('save', async function(next) {
-    // Only hash the password if it's modified (or new)
-    if (!this.isModified('password')) return next();
+userSchema.pre("save", async function (next) {
+  // Only hash the password if it's modified (or new)
+  if (!this.isModified("password")) return next();
 
-    try {
-      // Generate a salt
-      const salt = await bcrypt.genSalt(10);
+  try {
+    // Generate a salt
+    const salt = await bcrypt.genSalt(10);
 
-      // Hash password with the salt
-      this.password = await bcrypt.hash(this.password, salt);
-      next();
-    } catch (error) {
-      next(error);
-    }
-  });
+    // Hash password with the salt
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
 
-  // Method to check password validity
-  userSchema.methods.comparePassword = async function(candidatePassword) {
-    return bcrypt.compare(candidatePassword, this.password);
-  };
+userSchema.pre("find", function (next) {
+  // TODO:
+  // checks to see if current daily is correct
+  // if it is not correct, then update it by
+  // searching for the correct daily in the daily
+  // collection. If the daily does not exist,
+  // then create it as well.
+});
+
+// Method to check password validity
+userSchema.methods.comparePassword = async function (candidatePassword) {
+  return bcrypt.compare(candidatePassword, this.password);
+};
 
 const User = mongoose.model("User", userSchema);
 export default User;
