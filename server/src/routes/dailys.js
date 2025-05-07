43e92286bba201b0c811e27bd78994f8cb593cb1
ikealmighty <isaacyates7@gmail.com>
2025-05-06 19:15:30 -0500

    started implementing the follower dailys get route

--- server/src/routes/dailys.js
+++ server/src/routes/dailys.js
@@ -1,15 +1,40 @@
-import { Router } from 'express';
-import { ObjectId } from 'mongodb';
+import { Router } from "express";
+import Daily from "../models/daily";
+import User from "../models/user";
 const router = Router();
 
-router.get('/:username', (req, res) => {
-    const { user } = req;
-    const { username } = req.params;
-    const allDailys = Daily.find({ username });
+router.get("/:username", (req, res) => {
+  const { username } = req.params;
+  const allDailys = Daily.find({ username });
 
-    res.json(allDailys);
-})
+  res.json(allDailys);
+});
 
-// TODO: create similar routes for other daily related tasks the frontend will need
+router.get("/followers/:username/:dateEpochMs", (req, res) => {
+  const { username, dateEpochMs } = req.params;
+
+  let date = getMidnight(dateEpochMs);
+
+  let user = findOne({ username }).populate("followers");
+  let followers = user.followers;
+  followers.forEach((follower) => follower.populate("daily"));
+  // TODO: finish
+});
+
+router.get("/:username/:dateEpochMs", (req, res) => {
+  const { username, dateEpochMs } = req.params;
+
+  // find all dailys
+  let date = getMidnight(dateEpochMs);
+  const daily = Daily.find({ date: date.getTime(), username });
+
+  if (!daily) {
+    res
+      .status(404)
+      .json({ message: "No Daily found for given username and date" });
+  }
+
+  res.json(daily);
+});
 
 export default router;
