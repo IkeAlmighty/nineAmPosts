810c4bff9654b73d54f899acce1515c75e85bd46
ikealmighty <isaacyates7@gmail.com>
2025-05-06 19:16:19 -0500

    create a getMidnight function to compare date objects between db and client

---
+++ server/src/utils/datetime.js
@@ -0,0 +1,9 @@
+function getMidnight(epochMs) {
+  let date = new Date(epochMs);
+  date.setUTCHours(0);
+  date.setUTCMinutes(0);
+  date.setUTCSeconds(0);
+  date.setUTCMilliseconds(0);
+
+  return date;
+}
